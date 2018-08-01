81223a79d4b8e54ace3a75d7472bdd0a3a714acd
daidai <571143755@qq.com>
2018-08-01 17:00:27 +0800

    添加配置文件

---
+++ src/common/js/config.js
@@ -0,0 +1,1 @@
+export const avatar = 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1533019302401&di=ed0b72fd25e47aa8c540bccdbd072e6c&imgtype=0&src=http%3A%2F%2Ff.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2Fcaef76094b36acaf239dc5be7fd98d1001e99c76.jpg'
