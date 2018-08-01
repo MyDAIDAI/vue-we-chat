4c9507399f035851090aedf6cfda28a91a2f7563
daidai <571143755@qq.com>
2018-08-01 17:01:11 +0800

    聊天页面添加侧边栏

--- src/components/Talk.vue
+++ src/components/Talk.vue
@@ -1,14 +1,118 @@
 <template>
-  <layout></layout>
+  <layout @go="showCurrenList">
+    <list slot="list" :lists="listData" :type="listType"></list>
+  </layout>
 </template>
 
 <script>
 import Layout from '@/base/layout/Layout'
+import List from '@/base/list/List'
+import { avatar } from '@/common/js/config'
 export default {
   name: 'Talk',
-  components: { Layout },
+  components: { Layout, List },
   data () {
-    return {}
+    return {
+      listType: 'chat',
+      chatList: [
+        {
+          avatar: avatar,
+          nickname: '呆呆'
+        },
+        {
+          avatar: avatar,
+          nickname: '呆呆'
+        },
+        {
+          avatar: avatar,
+          nickname: '呆呆'
+        },
+        {
+          avatar: avatar,
+          nickname: '呆呆'
+        }
+      ],
+      contactList: [
+        {
+          letter: 'A',
+          contact: [
+            {
+              avatar: avatar,
+              nickname: '呆呆'
+            },
+            {
+              avatar: avatar,
+              nickname: '呆呆'
+            },
+            {
+              avatar: avatar,
+              nickname: '呆呆'
+            }
+          ]
+        },
+        {
+          letter: 'B',
+          contact: [
+            {
+              avatar: avatar,
+              nickname: '呆呆'
+            },
+            {
+              avatar: avatar,
+              nickname: '呆呆'
+            },
+            {
+              avatar: avatar,
+              nickname: '呆呆'
+            }
+          ]
+        },
+        {
+          letter: 'C',
+          contact: [
+            {
+              avatar: avatar,
+              nickname: '呆呆'
+            },
+            {
+              avatar: avatar,
+              nickname: '呆呆'
+            },
+            {
+              avatar: avatar,
+              nickname: '呆呆'
+            }
+          ]
+        },
+        {
+          letter: 'D',
+          contact: [
+            {
+              avatar: avatar,
+              nickname: '呆呆'
+            },
+            {
+              avatar: avatar,
+              nickname: '呆呆'
+            },
+            {
+              avatar: avatar,
+              nickname: '呆呆'
+            }
+          ]
+        }
+      ],
+      listData: []
+    }
+  },
+  created () {
+    this.listData = this.chatList
+  },
+  methods: {
+    showCurrenList (page) {
+      this.listType = page
+      this.listData = this[page + 'List']
+    }
   }
 }
 </script>
