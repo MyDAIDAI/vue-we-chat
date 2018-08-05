de941f6d2939069f4f9051d05e4334dd6d730bb3
daidai <571143755@qq.com>
2018-08-05 11:46:51 +0800

    聊天界面

--- src/components/Talk.vue
+++ src/components/Talk.vue
@@ -2,7 +2,27 @@
   <layout @go="showCurrenList">
     <list slot="list" :lists="listData" :type="listType"></list>
     <div ref="wrapper" class="chat-box">
-      <div class="srcoll-wrapper"></div>
+      <div class="srcoll-wrapper">
+        <div v-for="message in chatContent" :key="message.id">
+          <div class="message" :class="{'you': !message.isSend, 'me': message.isSend}">
+            <div class="message_system">
+              <div class="content">{{message.time}}</div>
+            </div>
+            <img :src="message.avatar" class="avatar"/>
+            <div class="content">
+              <div class="bubble" :class="{'right': message.isSend, 'left': !message.isSend, 'bubble_primary': message.isSend, 'bubble_default': !message.isSend}">
+                <div class="bubble_cont">
+                  <div class="plain">
+                    <pre class="message_plain">{{message.message}}</pre>
+                    <img src="" class="ico_loading"/>
+                    <!--<i class="ico_fail message_fail"></i>-->
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <div class="footer" slot="footer">
       <div class="toolbar">
@@ -120,7 +140,33 @@ export default {
         }
       ],
       listData: [],
-      preEditable: false
+      preEditable: false,
+      chatContent: [
+        {
+          time: '10:06',
+          avatar: avatar,
+          message: 'adsfadfas',
+          isSend: true
+        },
+        {
+          time: '10:06',
+          avatar: avatar,
+          message: 'adsfadfas',
+          isSend: false
+        },
+        {
+          time: '10:06',
+          avatar: avatar,
+          message: 'adsfadfas',
+          isSend: false
+        },
+        {
+          time: '10:06',
+          avatar: avatar,
+          message: 'adsfadfas',
+          isSend: true
+        }
+      ]
     }
   },
   created () {
@@ -142,6 +188,127 @@ export default {
     left: 0;
     right: 0;
     bottom: 180px;
+    padding: 0 19px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    .message {
+      margin-bottom: 16px;
+      float: left;
+      width: 100%;
+      &.me {
+        float: right;
+        text-align: right;
+        clear: right;
+        .avatar {
+          float: right;
+        }
+      }
+      .message_system {
+        text-align: center;
+        margin: 10px auto;
+        width: 100%;
+        .content {
+          display: inline-block;
+          font-size: 12px;
+          padding: 1px 18px;
+          color: #b2b2b2;
+          border-radius: 2px;
+        }
+      }
+      .avatar {
+        width: 40px;
+        height: 40px;
+        border-radius: 2px;
+        float: left;
+        cursor: pointer;
+      }
+      .content {
+        overflow: hidden;
+        .bubble {
+          max-width: 500px;
+          min-height: 1em;
+          display: inline-block;
+          vertical-align: top;
+          position: relative;
+          text-align: left;
+          font-size: 14px;
+          border-radius: 3px;
+          margin: 0 10px;
+          .bubble_cont {
+            word-wrap: break-word;
+            word-break: break-all;
+            min-height: 25px;
+            .plain {
+              padding: 9px 13px;
+              pre {
+                margin: 0;
+                white-space: pre-wrap;
+                word-break: normal;
+              }
+              .ico_loading {
+                position: absolute;
+                right: 100%;
+                top: 50%;
+                margin-top: -7px;
+                margin-right: 13px;
+              }
+              .ico_fail {
+                position: absolute;
+                right: 100%;
+                top: 50%;
+                margin-top: -11px;
+                margin-right: 9px;
+                cursor: pointer;
+              }
+              .message_fail {
+                display: inline-block;
+                vertical-align: middle;
+                width: 22px;
+                height: 22px;
+                background: url("../common/image/icon.png");
+                background-position: -289px -432px;
+                background-size: 487px 462px;
+              }
+            }
+            img {
+              vertical-align: middle;
+            }
+          }
+          &.bubble_primary {
+            background-color: #b2e281;
+          }
+          &.bubble_default {
+            background: #ffffff;
+          }
+          &:after, &:before{
+            position: absolute;
+            top: 14px;
+            border: 6px solid transparent;
+            content: '';
+          }
+          &.right {
+            &:before {
+              left: 100%;
+            }
+            &:after {
+              left: 100%;
+              border-left-color: #b2e281;
+              border-left-width: 4px;
+            }
+          }
+          &.left {
+            &:before {
+              right: 100%;
+            }
+            &:after {
+              right: 100%;
+              border-right-color: white;
+              border-right-width: 4px;
+            }
+          }
+        }
+      }
+    }
   }
   .footer {
     height: 180px;
