3eb3ae69cbfee444581e4e6eae4658e73cb5eae9
daidai <571143755@qq.com>
2018-10-30 15:38:01 +0800

    添加聊天记录表和请求好友数据表

---
+++ server/app/model/talk.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = app => {
+  const mongoose = app.mongoose;
+  const Schema = mongoose.Schema;
+
+  const TalkSchema = new Schema({
+    sendId: {
+      type: String
+    },
+    receiveId: {
+      type: String
+    },
+    sendStatus: {
+      type: Number,
+      default: 0
+    },
+    readStatus: {
+      type: Number,
+      default: 0
+    },
+    createTime: {
+      type: Date,
+      default: Date.now(),
+    },
+    updateTime: {
+      type: Date,
+      default: Date.now(),
+    },
+  });
+
+  return mongoose.model('Talk', TalkSchema);
+};
