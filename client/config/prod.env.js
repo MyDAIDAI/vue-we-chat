7c0e7e317cc3a6726b721e6773ace8f60e2aa8bd
dengpan <571143755@qq.com>
2019-09-28 10:08:20 +0800

    [build]

--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  socketHost: '"http://enable.dpdaidai.top:7001"', //wxy本地测试,
-  baseUrl: '"http://enable.dpdaidai.top:7001"'
+  socketHost: '"http://enable.dpdaidai.top:7002"', //wxy本地测试,
+  baseUrl: '"http://enable.dpdaidai.top:7002"'
 }
